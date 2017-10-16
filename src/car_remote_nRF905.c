4539e05abee08adc99d7d60c80807df14ddee56b
Zulolo <dodoleon@sina.com>
2017-10-16 20:54:04 +0800

    first commit

---
+++ src/car_remote_nRF905.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : car_remote_nRF905.c
+ Author      : ZK
+ Version     :
+ Copyright   : Your copyleft notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello my remote car!!!"); /* prints !!!Hello my remote car!!! */
+	return EXIT_SUCCESS;
+}
